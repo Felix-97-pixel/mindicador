84c9a0cc3988ad95e525e6bb95854c5cdbc50732
Felix Cabello Santelices <felix.cabello97@gmail.com>
2024-03-28 20:41:18 -0300

    ahora se dibujan los inputs desde las constantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
-
 import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 import { fetchData } from './api'; // Importa la función fetchData desde api.ts
-
-
 import Graficocomponent from './components/Grafico';
+import { EconomicIndicators, economicIndicators, Year, years, months } from './constantes/const'; // Importa las constantes
 
 const API_URL = 'https://mindicador.cl/api'; // Reemplaza esto con la URL de tu API
 
@@ -24,17 +22,26 @@ const App: React.FC = () => {
     fetchDataFromAPI();
   }, []);
 
+  const yearOptions = years.map((year: Year) => (
+    <option key={year} value={year}>{year}</option>
+  ));
+  const economicIndicatorsOptions = economicIndicators.map((economicIndicator: EconomicIndicators) => (
+    <option key={economicIndicator} value={economicIndicator}>{economicIndicator}</option>
+  ));
+
+  const monthOptions = Object.entries(months).map(([name, number]) => (
+    <option key={number} value={number}>{name}</option>
+));
+
   return (
     <Container>
       <Row>
         <Col>
           <InputGroup className="mb-3">
             <InputGroup.Text>Indicador</InputGroup.Text>
-            <FormControl as="select" id="inputGroupSelect01">
+            <FormControl as="select" id="economicIndicator">
               <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              {economicIndicatorsOptions}
             </FormControl>
           </InputGroup>
         </Col>
@@ -43,39 +50,35 @@ const App: React.FC = () => {
             <InputGroup.Text>Mes</InputGroup.Text>
             <FormControl as="select" id="inputGroupSelect02">
               <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              {monthOptions}
             </FormControl>
           </InputGroup>
         </Col>
         <Col>
           <InputGroup className="mb-3">
             <InputGroup.Text>Año</InputGroup.Text>
-            <FormControl as="select" id="inputGroupSelect03">
+            <FormControl as="select" id="year">
               <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              {yearOptions}
             </FormControl>
           </InputGroup>
         </Col>
       </Row>
       <Row>
-      <Col>
-      <Graficocomponent
-          x={["Enero", "Febrero", "Marzo", "Abril"]}
-          y={[10, 15, 13, 17]}
-          type="scatter"
-      />
-      </Col>
-      <Col>
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : (
-        <p>Cargando datos...</p>
-      )}
-      </Col>
+        <Col>
+          <Graficocomponent
+            x={["Enero", "Febrero", "Marzo", "Abril"]}
+            y={[10, 15, 13, 17]}
+            type="scatter"
+          />
+        </Col>
+        <Col>
+          {data ? (
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          ) : (
+            <p>Cargando datos...</p>
+          )}
+        </Col>
       </Row>
     </Container>
   );
---
+++ src/constantes/const.tsx
@@ -0,0 +1,42 @@
+export type EconomicIndicators = 'uf' | 'ivp' | 'dolar' | 'dolar_intercambio' | 'euro' | 'ipc' | 'utm' | 'imacec' | 'tpm' | 'libra_cobre' | 'tasa_desempleo' | 'bitcoin';
+export type Year = number;
+
+
+
+
+// Declarar el arreglo de nombres de datos
+export const economicIndicators: EconomicIndicators[] = [
+    'uf',
+    'ivp',
+    'dolar',
+    'dolar_intercambio',
+    'euro',
+    'ipc',
+    'utm',
+    'imacec',
+    'tpm',
+    'libra_cobre',
+    'tasa_desempleo',
+    'bitcoin'
+];
+
+// Declarar el arreglo de años
+export const years: Year[] = [];
+for (let year = 1990; year <= 2024; year++) {
+    years.push(year);
+}
+
+export const months: { [key: string]: number } = {
+    Enero: 1,
+    Febrero: 2,
+    Marzo: 3,
+    Abril: 4,
+    Mayo: 5,
+    Junio: 6,
+    Julio: 7,
+    Agosto: 8,
+    Septiembre: 9,
+    Octubre: 10,
+    Noviembre: 11,
+    Diciembre: 12
+};
