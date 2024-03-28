1fe5cea951a16ab8896a91df443f8ce9b0e73a95
Felix Cabello Santelices <felix.cabello97@gmail.com>
2024-03-28 01:23:07 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+
+import React, { useState, useEffect } from 'react';
+import { fetchData } from './api'; // Importa la función fetchData desde api.ts
+
+const API_URL = 'https://mindicador.cl/api'; // Reemplaza esto con la URL de tu API
+
+const App: React.FC = () => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const jsonData = await fetchData(API_URL);
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchDataFromAPI();
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Indicador</InputGroup.Text>
+            <FormControl as="select" id="inputGroupSelect01">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </FormControl>
+          </InputGroup>
+        </Col>
+        <Col>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Mes</InputGroup.Text>
+            <FormControl as="select" id="inputGroupSelect02">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </FormControl>
+          </InputGroup>
+        </Col>
+        <Col>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Año</InputGroup.Text>
+            <FormControl as="select" id="inputGroupSelect03">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </FormControl>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+      <Col>
+        asdasdasd
+      </Col>
+      <Col>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Cargando datos...</p>
+      )}
+      </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/api.ts
@@ -0,0 +1,13 @@
+export async function fetchData(url: string): Promise<any> {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const jsonData = await response.json();
+        return jsonData;
+      } else {
+        throw new Error('Error al obtener datos de la API');
+      }
+    } catch (error) {
+      throw new Error('Error al realizar la solicitud a la API');
+    }
+  }
